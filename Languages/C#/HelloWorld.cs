9b756057239c47532d8bbc1db98d652624af6d0f
rushabhshah309 <67695243+rushabhshah309@users.noreply.github.com>
2020-10-08 18:28:01 +0530

    Hello World in C#
    Added Hello World Program in C# language.

---
+++ Languages/C#/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace HelloWorldApp { 
+      
+    class HelloWorld { 
+    
+        static void Main(string[] args) { 
+              
+            Console.WriteLine("Hello World!!"); 
+
+            Console.ReadKey(); 
+        } 
+    } 
+} 
